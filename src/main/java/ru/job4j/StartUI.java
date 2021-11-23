9e69d8445134977ca178fbecaa705cf80456c1ce
Artem <majehed89@gmail.com>
2021-11-23 22:02:17 +0500

    6. Date. Отображение даты. [#310017]

---
+++ src/main/java/ru/job4j/StartUI.java
@@ -0,0 +1,16 @@
+package ru.job4j;
+
+import ru.job4j.tracker.Item;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item item = new Item();
+        LocalDateTime Item = item.getCreated();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String currentDateTimeFormat = Item.format(formatter);
+        System.out.println("Текущие дата и время после форматирования: " + currentDateTimeFormat);
+    }
+}
