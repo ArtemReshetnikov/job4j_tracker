511afc03ef901e0ed1f668e2a5fe2cd1883112e8
ArtemReshetnikov <majehed89@gmail.com>
2021-11-28 21:32:42 +0500

    9. toString [#310018]

--- src/main/java/ru/job4j/StartUI.java
+++ src/main/java/ru/job4j/StartUI.java
@@ -1,16 +1,12 @@
 package ru.job4j;
 
 import ru.job4j.tracker.Item;
-
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class StartUI {
     public static void main(String[] args) {
-        Item time = new Item();
-        LocalDateTime now = time.getCreated();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        String currentDateTimeFormat = now.format(formatter);
-        System.out.println("Текущие дата и время после форматирования: " + currentDateTimeFormat);
+        Item item = new Item("Artem", 1);
+        System.out.println(item);
     }
 }
